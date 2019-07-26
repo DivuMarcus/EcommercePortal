584a7ce18a878608fb313ee0bccde1429f8fb7a7
Divumarcus <98.divyansh@gmail.com>
2019-07-27 00:52:28 +0530

    Frontend converted in File and all connections updated

--- routes/about.js
+++ routes/about.js
@@ -1,11 +1,6 @@
 var router = require('express').Router();
 
-
 router.get('/',function(req,res){
-  res.render('main/home');
-});
-
-router.get('/about',function(req,res){
   res.render('main/about');
 });
 
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var User = require('./models/user');
 
 var app = express();
 
-var password = 'ecommerce123'
+var password = 'ecommerce123';
 
 mongoose.connect('mongodb+srv://tcs-ecommerce:' + password + '@ecommerce-tcs-qqgie.mongodb.net/test?retryWrites=true&w=majority',{
   useNewUrlParser:true
@@ -30,10 +30,25 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.engine('ejs',ejs_mate);
 app.set('view engine','ejs');
 
-var mainRoutes = require('./routes/main');
+var homeRoutes = require('./routes/home');
+var aboutRoutes =require('./routes/about');
 var userRoutes = require('./routes/user');
-app.use('/main',mainRoutes);
+var cartRoutes = require('./routes/cart');
+var checkoutRoutes = require('./routes/checkout');
+var thankyouroutes = require('./routes/thankyou');
+var shopRoutes = require('./routes/shop');
+var shopsingleroutes = require('./routes/shop-single');
+var contactRoutes = require('./routes/contact');
+
+app.use('/home',homeRoutes);
+app.use('/about',aboutRoutes);
 app.use('/user',userRoutes);
+app.use('/cart',cartRoutes);
+app.use('/checkout',checkoutRoutes);
+app.use('/thankyou',thankyouroutes);
+app.use('/shop',shopRoutes);
+app.use('/shop-single',shopsingleroutes);
+app.use('/contact',contactRoutes);
 
 app.listen(3000, function(err){
   if (err) throw err;
